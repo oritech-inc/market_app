46ef9b57db284750e80466b86989e72a4209fb68
Lutho Xuba <lutho@freshmarksystems.co.za>
2023-05-19 15:11:11 +0200

    Changes by Lutho

--- client/src/components/layouts/links/Quote.jsx
+++ client/src/components/layouts/links/Quote.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react"
 import Axios from "axios"
 import {
-  Box, Divider, VStack, Spacer, Button, CardFooter, HStack, Card, CardHeader, CardBody,
+  Box, Tooltip, Select, Divider, VStack, Spacer, Button, CardFooter, HStack, Card, CardHeader, CardBody,
   Kbd, Input, Text, Badge, Grid, GridItem, NumberInput, NumberInputField, NumberDecrementStepper, NumberIncrementStepper,
   NumberInputStepper, Table, Thead, Tbody, TableContainer, Tr, Th, Td, TableCaption,
 } from "@chakra-ui/react"
@@ -10,60 +10,70 @@ import { MdAdd, MdArrowForward } from "react-icons/md"
 
 function App() {
   let ob = [];
-  const [data1, setData] = useState([]);
-  const [market, setMarket] = useState("");
+  const [markets, setMarkets] = useState([]);
   const [commodities, setCommodities] = useState([]);
   const [products, setProducts] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:5008/markets")
-  //     .then((res) => res.json())
-  //     .then((data1) => setData(data1))
-  //     .catch((err) => console.log(err));
-  // }, []);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:5008/products")
-  //     .then((res) => res.json())
-  //     .then((products) => setProducts(products))
-  //     .catch((err) => console.log(err));
-  // }, []);
+  const [itemCode, setItemCode] = useState("");
+  const [totmass, setTotMass] = useState("");
 
   useEffect(() => {
-    const fetchCommodities = async () => {
-      let postFormData = new FormData();
-
-      postFormData.append("query", "item-stats");
-      postFormData.append("device", "9c2807cb-1e06-4fae-8a43-418189035eca");
-      postFormData.append("market", 17);
-      postFormData.append("item_code", "ON");
-      postFormData.append("api_key", "2766D02F-D00D-B0FD-FDB5-3C310DFB9964");
-      postFormData.append(
-        "event_id",
-        "GkPLQfMjImbCgq7Dyfb4W4Pi9jrx24q2YgkXQqQF"
-      );
-
-      const response = await fetch(
-        "https://webservices.freshmarksystems.co.za/mobiapi/market-query",
-        {
-          method: "POST",
-          body: postFormData,
-        }
-      );
-
-      const responseData = await response.json();
-      const data = responseData.result.trans;
-      setCommodities(data);
-      console.log(commodities);
-    };
-    fetchCommodities();
+    fetch("http://localhost:5009/markets")
+      .then((res) => res.json())
+      .then((markets) => setMarkets(markets))
+      .catch((err) => console.log(err));
   }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:5009/products")
+      .then((res) => res.json())
+      .then((products) => setProducts(products))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    if (itemCode.length > 1) {
+      const fetchCommodities = async () => {
+        let postFormData = new FormData();
+
+        postFormData.append("query", "item-stats");
+        postFormData.append("device", "9c2807cb-1e06-4fae-8a43-418189035eca");
+        postFormData.append("market", 17);
+        postFormData.append("item_code", itemCode);
+        postFormData.append("api_key", "2766D02F-D00D-B0FD-FDB5-3C310DFB9964");
+        postFormData.append(
+          "event_id",
+          "GkPLQfMjImbCgq7Dyfb4W4Pi9jrx24q2YgkXQqQF"
+        );
+
+        const response = await fetch(
+          "https://webservices.freshmarksystems.co.za/mobiapi/market-query",
+          {
+            method: "POST",
+            body: postFormData,
+          }
+        );
+
+        const responseData = await response.json();
+        const data = responseData.result.trans;
+        setCommodities(data);
+        console.log("test1");
+        console.log(commodities);
+      };
+      fetchCommodities();
+    }
+  }, [itemCode]);
 
   const handleClick = (e) => {
     e.preventDefault();
     alert('clicked');
 
   };
+
+  const handleChangeItems = (e) => {
+    e.preventDefault();
+    setItemCode(e.target.value);
+  };
+
   const handleArray = (e) => {
     e.preventDefault();
     const id = e.target.value;
@@ -79,9 +89,52 @@ function App() {
 
   return (
     <>
+      <Box m={0} p={0} w="100%" bg="white.500">
+        <Tooltip
+          hasArrow
+          placement="bottom"
+          label="Select Market from the list"
+          aria-label="Average"
+        >
+          <Select
+            size={"lg"}
+            placeholder="-- Select Market --"
+            variant="filled"
+
+          >
+            {markets.map((d, i) => (
+              <option key={i} value={d.id}>
+                {d.name}
+              </option>
+            ))}
+          </Select>
+        </Tooltip>
+      </Box>
+
+      <Box m={0} p={0} w="100%" bg="white.500">
+        <Tooltip
+          hasArrow
+          placement="bottom"
+          label="Select Item"
+          aria-label="Average"
+        >
+          <Select
+            size={"lg"}
+            placeholder="-- Select Item --"
+            variant="filled"
+            onChange={handleChangeItems}
+          >
+            {products.map((d, i) => (
+              <option key={i} value={d.item_code}>
+                {d.item}
+              </option>
+            ))}
+          </Select>
+        </Tooltip>
+      </Box>
       <Box m={0} p={0} w="100%" bg="">
         {commodities.map((d, i) => (
-          <span key={i} id="market-data1">
+          <span key={i} id="market-markets">
             <VStack py={1} spacing="1" border={"1xp"}>
               <Card borderColor="black"
                 w={"97%"} h={"100%"} my={2} borderRadius={"lg"}
@@ -92,7 +145,7 @@ function App() {
                       variant={"solid"} py={2} size={'sm'}
                       colorScheme="teal"
                     >
-                    {d.commodity}
+                      {d.commodity}
                     </Badge>
                   </GridItem>
                 </Grid>
@@ -105,7 +158,7 @@ function App() {
                     Average: R{d.average.toFixed(2)}
                   </Text>
                 </CardHeader>
-                <Divider/>
+                <Divider />
                 <Box>
                   <TableContainer>
                     <Table variant='striped' colorScheme='teal' size='md'>
